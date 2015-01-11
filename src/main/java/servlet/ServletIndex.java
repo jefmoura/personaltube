c0d26407e33b9aeee743921251f3a876844f2a87
Jeferson Moura <jefmmoreira@gmail.com>
2015-01-11 17:50:01 -0200

    Primeiro commit com algumas coisas ja implementadas

---
+++ src/main/java/servlet/ServletIndex.java
@@ -0,0 +1,35 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import controller.AmazonS3Upload;
+import controller.ZencoderEncode;
+
+/**
+ *
+ * @author jeferson
+ */
+public class ServletIndex extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Jan 10, 2015, 7:10:45 PM
+    Author     : jeferson
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
